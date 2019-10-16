0c98a5cc7e20c964d3aca87752ba44503db4f0cd
Matthew Chang <matt@Matthews-MacBook-Pro.local>
2019-10-16 10:09:07 -0700

    Backend Final Pushed

--- FullStack/core/api.py
+++ FullStack/core/api.py
@@ -1,60 +1,50 @@
+from flask import Response
 from flask import request
 from flask_restful import Resource
+from flask_mail import Message
+from FullStack import mail
 #from FullStack import db
 #from firebase_admin import firestore
 
 class incidentAPI(Resource):
     def post(self):
-        try:
-            #Load json
-            incidentData = requent.json['body']
-            location = incidentData['location']
-            time = incidentData['time']
-            types = incidentData['types']
-            link = incidentData['link']
-            start = incidentData['start']
-            end = incidentData['end']
-            username = incidentData['username']
-            contact = incidentData['contact']
+        #Load json
+        incidentData = request.json
+        location = incidentData['location']
+        time = incidentData['time']
+        types = incidentData['types']
+        link = incidentData['link']
+        start = incidentData['start']
+        end = incidentData['end']
+        username = incidentData['username']
+        contact = incidentData['contact']
 
-            #Enter incdent into database
-            #user_ref = db.collection(u'users').document(username)
-            #user_ref.update({u'incidents': firestore.ArrayUnion([incidentData])})
+        #Enter incdent into database
+        #user_ref = db.collection(u'users').document(username)
+        #user_ref.update({u'incidents': firestore.ArrayUnion([incidentData])})
 
-            #Contact Enformcement
-            msg = Message(subject="EMERGENCY", sender='dubhacks.rs@gmail.com', recipients=['dubhacks.enforcement@gmail.com'])
-            msg.body = """
-            Hello Enforcement,
+        #Contact Enformcement
+        msg = Message(subject="EMERGENCY", sender='dubhacks.rs@gmail.com', recipients=['dubhacks.enforcement@gmail.com'])
+        msg.body = """
+        Hello Enforcement,
 
-            EMERGENCY: There is a potential gun violence incident!
-            Here are the details:
-            Location of Incident:
-            %s
+        There is a potential gun violence incident! Here are the details:
 
-            Time of Incident:
-            %s
+        Location of Incident: %s
+        Time of Incident: %s utc
+        Firearm Type(s): %s
+        Video Footage: %s
+        Video Footage Start: %s
+        Video Footage End: %s
+        Primary Owner: %s
+        Primary Owner Contact: %s
 
-            Firearm Type(s):
-            %s
+        Sincerly,
 
-            Video Footage:
-            %s
+        Rapid Security
+        """ % (location, time ,types, link, start, end, username, contact)
+        mail.send(msg)
 
-            Video Footage Start:
-            %s
-
-            Video Footage End:
-            %s
-
-            Primary Owner:
-            %s
-
-            Primary Owner Contact:
-            %s
-            """ % (location,time ,types, firearm, link, start, end, username, contact)
-            mail.send(msg)
-
-            return f"Success"
-
-        except Exception as e:
-            return f"An Error Occured: {e}"
+        #resp = Response({"msg":msg})
+        #resp.headers['Access-Control-Allow-Origin'] = '*'
+        #return resp
